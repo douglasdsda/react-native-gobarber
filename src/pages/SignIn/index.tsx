53cb9f066da41c2607d5be5bcaf90d856c662778
douglasdsda <douglasdsda@gmail.com>
2020-07-12 08:44:53 -0300

    botao input

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="email" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('click');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
